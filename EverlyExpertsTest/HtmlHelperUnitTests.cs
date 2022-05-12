42e02a3e60c87e2c61c1464ec35cd43ed084eeb4
iconofcoil <luispablo.hg@gmail.com>
2022-05-11 19:41:12 -0500

    Completed unit tests for HtmlHelper methods

--- EverlyExpertsTest/HtmlHelperUnitTests.cs
+++ EverlyExpertsTest/HtmlHelperUnitTests.cs
@@ -1,5 +1,6 @@
 using EverlyExperts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EverlyExpertsTest
@@ -18,6 +19,31 @@ namespace EverlyExpertsTest
 
             // Assert
             Assert.AreEqual(resultUrl, "https://stackoverflow.com/questions/70592774/azure-function-to-signalr-serverless-to-web-app");
+        }
+
+        [TestMethod]
+        public async Task TestParseHtmlHeadings1to3Method()
+        {
+            // Arrange
+            string url = "https://web.ics.purdue.edu/~gchopra/class/public/pages/webdesign/05_simple.html";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("A very simple webpage. This is an \"h1\" level header.");
+            sb.Append(" ");
+            sb.Append("This is a level h2 header.");
+            sb.Append(" ");
+            sb.Append("How about a nice ordered list!");
+            sb.Append(" ");
+            sb.Append("Unordered list");
+            sb.Append(" ");
+            sb.Append("Nested Lists!");
+
+            // Act
+            string resultUrl = await HtmlHelper.ParseHtmlHeadings1to3(url);
+
+            // Assert
+            Assert.AreEqual(resultUrl, sb.ToString());
         }
     }
 }
