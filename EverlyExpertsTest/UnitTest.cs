9e60c0c9636735ec14f67e92132e861e3de8be62
iconofcoil <luispablo.hg@gmail.com>
2022-05-11 19:15:31 -0500

    Added Unit Testing Project

---
+++ EverlyExpertsTest/UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EverlyExpertsTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
